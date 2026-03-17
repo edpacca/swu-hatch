f6c81297e429cdb7e12bbbb078c36fc2ee4b2304
Eddie Pace <eddie@pace.me.uk>
2026-03-17 15:49:14 +0000

    make 3% more attractive

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-    import { decodeWav, freeWav } from './decode.js';
+    import { decodeWav, freeWav } from "./decode.js";
 
-    let output = $state('');
+    let output = $state("");
 
     async function handleFile(e) {
         const file = e.target.files[0];
@@ -13,6 +13,27 @@
     }
 </script>
 
-<h1>SWU Hatch</h1>
-<input type="file" accept=".wav" onchange={handleFile} />
-<div>{output}</div>
+<main>
+    <h1>SWU Hatch</h1>
+    <input type="file" accept=".wav" onchange={handleFile} />
+    <div>{output}</div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        height: calc(100vh - 16px);
+        width: 100%;
+        align-items: center;
+        background-color: darkblue;
+        color: lightblue;
+        font-family: sans-serif;
+    }
+
+    h1 {
+        font-family: sans-serif;
+        color: lightblue;
+    }
+</style>
